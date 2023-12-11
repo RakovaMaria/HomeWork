f521da673f81478c3d855b710df8192779827dbd
RakovaMaria <m98535248@gmail.com>
2023-12-11 20:16:42 +0300

    Удален файл ghjdthrf.py

--- HomeWork/HW2/ghjdthrf.py
+++
@@ -1,6 +0,0 @@
-import os
-print(os.getcwd())
-cd C:\Users\Мария\PycharmProjects\pythonProject\HomeWork\HW2
-git rm "Заглавные и прописные символы.py"
-git commit -m "Удален файл Заглавные и прописные символы.py"
-git push
